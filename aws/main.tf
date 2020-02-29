7a925779bff5f4b7c1af17e48d10965bb402510d
Sandeep Shantharam <machbio@gmail.com>
2020-02-29 10:37:37 -0500

    Added Depends on SES

--- aws/main.tf
+++ aws/main.tf
@@ -2,4 +2,7 @@ module "ses" {
   source = "./ses"
   domains = var.domains
   www_domains = var.www_domains
+
+  depends_on = [aws_route53_zone.xgd_domain, aws_route53_zone.biohole_domain,
+   aws_route53_zone.machbio_domain, aws_route53_zone.desinle_domain]
 }
